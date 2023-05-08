9217e1cbff11eeb563feec711cd5a7f17c2cecb4
AbGhost-cyber <xplendo@gmail.com>
2023-05-08 22:57:20 +0800

    commit

--- playground2/part4.py
+++ playground2/part4.py
@@ -1,4 +1,77 @@
 import torch
+import matplotlib.pyplot as plt
+
+#  A hot problem, chapter 5
+t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
+t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
+
+t_c = torch.tensor(t_c)
+t_u = torch.tensor(t_u)
+
+
+# we assume that our unknown data might follow a linear model
+def model(t_u, w, b):
+    return w * t_u + b
+
+
+def loss_fn(t_p, t_c):
+    squared_diffs = (t_p - t_c) ** 2
+    return squared_diffs.mean()
+
+
+w = torch.ones(())
+b = torch.zeros(())
+
+t_p = model(t_u, w, b)
+
+delta = 0.1
+
+loss_rate_of_change_w = \
+    (loss_fn(model(t_u, w + delta, b), t_c) -
+     loss_fn(model(t_u, w - delta, b), t_c)) / (2.0 * delta)
+learning_rate = 1e-2
+w = w - learning_rate * loss_rate_of_change_w
+loss_rate_of_change_b = \
+    (loss_fn(model(t_u, w, b + delta), t_c) -
+     loss_fn(model(t_u, w, b - delta), t_c)) / (2.0 * delta)
+b = b - learning_rate * loss_rate_of_change_b
+
+
+# derivative loss func
+def dloss_fn(t_p, t_c):
+    dsq_diffs = 2 * (t_p - t_c) / t_p.size(0)
+    return dsq_diffs
+
+
+def dmodel_dw(t_u, w, b):
+    return t_u
+
+
+def dmodel_db(t_u, w, b):
+    return 1.0
+
+
+def grad_fn(t_u, t_c, t_p, w, b):
+    dloss_dtp = dloss_fn(t_p, t_c)
+    dloss_dw = dloss_dtp * dmodel_dw(t_u, w, b)
+    dloss_db = dloss_dtp * dmodel_db(t_u, w, b)
+    return torch.stack([dloss_dw.sum(), dloss_db.sum()])
+
+
+def training_loop(n_epochs, learning_rate, params, t_u, t_c):
+    for epoch in range(1, n_epochs + 1):
+        w, b = params
+
+        t_p = model(t_u, w, b)  # forward pass
+        loss = loss_fn(t_p, t_c)
+        grad = grad_fn(t_u, t_c, t_p, w, b)  # backward pass
+
+        params = params - learning_rate * grad
+
+        print('Epoch %d, Loss %f' % (epoch, float(loss)))  # <3>
+
+    return params
+
 
 if __name__ == '__main__':
-    print(ord(""))
+    print()
